a243e24ae8a0f95ec6d1a2537abfb0a330b2b0bd
jeonghoon11 <jih361@naver.com>
2025-09-16 13:54:39 +0000

    Merge pull request #337 from Passtival/develop
    Production-release(v.1.0.0)

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
